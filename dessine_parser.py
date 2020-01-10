87589b243552397bd34db5ea0b7e3a04dd9edd6c
Bürki Pierre <pierre.burki@he-arc.ch>
2020-01-10 14:54:43 +0100

    Rename and clean parser.py
    Co-authored-by: Jeanneret Loïck <lock.jeanneret@gmail.com>

--- dessine_parser.py
+++ dessine_parser.py
@@ -1,13 +1,22 @@
 import ply.yacc as yacc
 import AST
 from lex import tokens
+import logger
+import sys
 
+###########################################
+# DesSine parser
+# Made by Pierre Bürki and Loïck Jeanneret
+# Last updated on 10.01.20
+###########################################
 
+# Program must always start with an init block
 def p_program(p):
     '''program : init_block newline body'''
     p[0] = AST.ProgramNode([p[1], p[3]])
 
 
+# Init block must contain at least one instruction
 def p_init_block(p):
     '''init_block : init 
         | init_block newline init'''
@@ -18,6 +27,7 @@ def p_init_block(p):
     p[0] = AST.InitBlockNode(children)
 
 
+# An init instruction consists of an init function call (e.g. #width(100))
 def p_init(p):
     '''init : INIT_PREFIX INIT_FUNCTION PARENTHESIS_OPEN parameters PARENTHESIS_CLOSE'''
     p[0] = AST.InitNode(p[2], p[4])
@@ -31,36 +41,36 @@ def p_body(p):
     except:
         p[0] = AST.BodyNode(p[1])
 
+
+# The next two rules are used to "absorb" new lines surrounding the body
 def p_reduce_left_body(p):
     '''body : newline body'''
     p[0] = p[2]
 
+
 def p_reduce_right_body(p):
     '''body : body newline'''
     p[0] = p[1]
-    
 
+# Syntaxic sugar allowing us to use "empty" instead of not writing anything
 def p_empty(p):
     'empty :'
     pass
 
-# If empty does not work, copy rule without parameters or make something between
-
-
+# Parameters can be empty, contain one expression, or a list of expression separated by commas
 def p_parameters(p):
     '''parameters : expression
         | expression COMMA parameters'''
     try:
         p[0] = [p[1]] + p[3]
     except:
-        try:
-            p[0] = [p[1]]
-        except:
-            p[0] = []
+        p[0] = [p[1]]
 
+# For lisibility's sake
 def p_empty_parameters(p):
     'parameters : empty'
     p[0] = []
+
 
 def p_statement(p):
     '''statement : assignment
@@ -80,7 +90,9 @@ def p_structure(p):
         | for'''
     p[0] = p[1]
 
-
+# if and if...else blocks
+# Note that the condition must be a comparison (e.g. if(a == 1) works, but if(a) does not)
+# Note that brackets must be on the same line
 def p_if(p):
     '''if : IF PARENTHESIS_OPEN comparison PARENTHESIS_CLOSE BRACKET_OPEN body BRACKET_CLOSE
         | IF PARENTHESIS_OPEN comparison PARENTHESIS_CLOSE BRACKET_OPEN body BRACKET_CLOSE ELSE BRACKET_OPEN body BRACKET_CLOSE'''
@@ -89,17 +101,17 @@ def p_if(p):
     except:
         p[0] = AST.IfNode([p[3], p[6]])
 
-
+# Note that the condition must be a comparison (e.g. while(a == 1) works, but while(a) does not)
 def p_while(p):
     '''while : WHILE PARENTHESIS_OPEN comparison PARENTHESIS_CLOSE BRACKET_OPEN body BRACKET_CLOSE'''
     p[0] = AST.WhileNode([p[3], p[6]])
 
-
+# Note that the condition must be a comparison
 def p_for(p):
     'for : FOR PARENTHESIS_OPEN assignment SEMICOLON comparison SEMICOLON assignment PARENTHESIS_CLOSE BRACKET_OPEN body BRACKET_CLOSE'
     p[0] = AST.ForNode([p[3], p[5], p[7], p[10]])
 
-
+# An action returns nothing
 def p_action(p):
     'action : BUILTIN_ACTION PARENTHESIS_OPEN parameters PARENTHESIS_CLOSE'
     p[0] = AST.FunctionNode(p[1], p[3])
@@ -109,7 +121,7 @@ def p_assignment(p):
     'assignment : IDENTIFIER EQUAL expression'
     p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
 
-
+# Starting here, all the different expression types
 def p_expression_num(p):
     '''expression : NUMBER
         | HEX_NUMBER'''
@@ -154,10 +166,15 @@ def p_expression_paren(p):
     'expression : PARENTHESIS_OPEN expression PARENTHESIS_CLOSE'
     p[0] = p[2]
 
-
+# The parser exits whenever it encounters an error
 def p_error(p):
-    print(f"Syntax error in line {p.lineno}")
-    yacc.errok()
+    try:
+        logger.error("Parser error", p.lineno, f"Unexpected token '{p.value}'")
+    except:
+        # p is None => end of file reached
+        logger.error("Parser error", "EOF", "Couldn't parse program, check for missing brackets / parenthesis")
+    
+    sys.exit(-1)
 
 
 yacc.yacc(outputdir='')
@@ -168,8 +185,8 @@ def parse(program):
 
 
 if __name__ == "__main__":
-    import sys
     import os
+
     with open(sys.argv[1]) as prog:
         result = yacc.parse(prog.read(), debug=True)
         print(result)
@@ -177,3 +194,4 @@ if __name__ == "__main__":
         name = os.path.splitext(sys.argv[1])[0] + '-ast.pdf'
         graph.write_pdf(name)
         print("wrote ast to ", name)
+        
